0aa3637398c002515122a92263b29afbbb9ad4e2
Mart-Jan Den Hollander <martjandenhollander@gmail.com>
2022-11-27 20:14:00 +0100

    Solution to exercise 4.

--- Quiz4Exercise3/Program.cs
+++ Quiz4Exercise3/Program.cs
@@ -2,7 +2,7 @@
 
 string myString = "Let's print all Words with Upper caseS";
 
-var output = myString.Split(' ')
+var output = myString   .Split(' ')
                         .Where(x => x.Any(y => Char.IsUpper(y)))
                         .ToList();
 
---
+++ Quiz4Exercise4/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Linq;
+
+int[] myArray = new int[10] {2, 1, 4, 11, 2, 1, 45, 2, 3, 7 };
+
+int min = 2;
+int max = 15;
+
+myArray .Where(x => x >= min && x <= max)
+        .OrderBy(x => x)
+        .ToList()
+        .ForEach(x => Console.WriteLine(x));
