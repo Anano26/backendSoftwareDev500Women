0a9660dcbfb72c6142725a0a17934ffa4833269d
Mart-Jan Den Hollander <martjandenhollander@gmail.com>
2022-11-27 19:53:19 +0100

    Solution exercise 2

---
+++ Quiz4Exercise2/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+string myString = "Let's count characters";
+
+var countPerElement = myString.GroupBy(x => x)
+                            .ToDictionary(x => x.Key, x => x.Count())
+                            .ToList();
+
+countPerElement.ForEach(x => Console.WriteLine(x.Key.ToString() + ": " + x.Value.ToString() + " times"));
