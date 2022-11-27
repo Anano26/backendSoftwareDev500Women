c690af93de8e943a93a4cbd29bb225a9266a61c6
Mart-Jan Den Hollander <martjandenhollander@gmail.com>
2022-11-27 20:33:12 +0100

    Solution exercise 5

---
+++ Quiz4Exercise5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+int[] ints = new int[10] { 3, 5, 5, 2, 3, 5, 4, 2, 28, 3 };
+
+var countPerElement = ints.GroupBy(x => x)
+                            .ToDictionary(x => x.Key, x => x.Count());
+
+countPerElement .Where(x => x.Value == countPerElement.Values.Max())
+                .Select(x => x.Key)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+
