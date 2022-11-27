34fc70f476935b8e55d4b5fff7de0b69f30f20c0
Mart-Jan Den Hollander <martjandenhollander@gmail.com>
2022-11-27 20:07:36 +0100

    Solution exercise 3

---
+++ Quiz4Exercise3/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+string myString = "Let's print all Words with Upper caseS";
+
+var output = myString.Split(' ')
+                        .Where(x => x.Any(y => Char.IsUpper(y)))
+                        .ToList();
+
+output.ForEach(x => Console.WriteLine(x));
